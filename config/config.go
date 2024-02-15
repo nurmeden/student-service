784d980eba57453d77afb79a8338b55374921edf
nurmeden <nurmeden.02@gmail.com>
2024-02-15 12:18:39 +0600

    feat(Config): Загрузка и парсинг файла конфигурации

--- config/config.go
+++ config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"errors"
+	"github.com/spf13/viper"
+	"log"
+)
+
 type Config struct {
 	JWTKey string `env:"JWT_KEY" envDefault:"supersecret"`
 	Logger Logger
@@ -22,4 +28,34 @@ type Database struct {
 	DBPort         string `env:"MONGO_PORT" envDefault:"27017"`
 	DBName         string `env:"DATABASE_NAME" envDefault:"studentsdb"`
 	CollectionName string `env:"DATABASE_NAME" envDefault:"students"`
+}
+
+func LoadConfig(filename string) (*viper.Viper, error) {
+	v := viper.New()
+
+	v.SetConfigName(filename)
+	v.AddConfigPath(".")
+	v.AutomaticEnv()
+
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("config file not found")
+		}
+
+		return nil, err
+	}
+
+	return v, nil
+}
+
+func ParseConfig(v *viper.Viper) (*Config, error) {
+	var c Config
+
+	err := v.Unmarshal(&c)
+	if err != nil {
+		log.Printf("unable to decode into struct, %v", err)
+		return nil, err
+	}
+
+	return &c, err
 }
