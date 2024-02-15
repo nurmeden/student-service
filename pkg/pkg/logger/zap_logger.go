4191c55800f8a231b4a1527c0193addaea53f8a8
nurmeden <nurmeden.02@gmail.com>
2024-02-15 12:43:27 +0600

    feat(Logger): Логгер файл

---
+++ pkg/pkg/logger/zap_logger.go
@@ -0,0 +1,70 @@
+package logger
+
+import (
+	"github.com/nurmeden/students-service/config"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+type Logger interface {
+}
+
+type apiLogger struct {
+	cfg         *config.Config
+	sugarLogger *zap.SugaredLogger
+}
+
+var loggerLevelMap = map[string]zapcore.Level{
+	"debug":  zapcore.DebugLevel,
+	"info":   zapcore.InfoLevel,
+	"warn":   zapcore.WarnLevel,
+	"error":  zapcore.ErrorLevel,
+	"dpanic": zapcore.DPanicLevel,
+	"panic":  zapcore.PanicLevel,
+	"fatal":  zapcore.FatalLevel,
+}
+
+func (l *apiLogger) getLoggerLevel(cfg *config.Config) zapcore.Level {
+	level, exist := loggerLevelMap[cfg.Logger.Level]
+	if !exist {
+		return zapcore.DebugLevel
+	}
+
+	return level
+}
+
+func (l *apiLogger) InitLogger() {
+	logLevel := l.getLoggerLevel(l.cfg)
+
+	logWriter := zapcore.AddSync(os.Stderr)
+
+	var encoderCfg zapcore.EncoderConfig
+	if l.cfg.Server.Mode == "Development" {
+		encoderCfg = zap.NewDevelopmentEncoderConfig()
+	} else {
+		encoderCfg = zap.NewProductionEncoderConfig()
+	}
+
+	var encoder zapcore.Encoder
+	encoderCfg.LevelKey = "LEVEL"
+	encoderCfg.CallerKey = "CALLER"
+	encoderCfg.TimeKey = "TIME"
+	encoderCfg.NameKey = "NAME"
+	encoderCfg.MessageKey = "MESSAGE"
+
+	if l.cfg.Logger.Encoding == "console" {
+		encoder = zapcore.NewConsoleEncoder(encoderCfg)
+	} else {
+		encoder = zapcore.NewJSONEncoder(encoderCfg)
+	}
+
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+	core := zapcore.NewCore(encoder, logWriter, zap.NewAtomicLevelAt(logLevel))
+	logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
+
+	l.sugarLogger = logger.Sugar()
+	if err := l.sugarLogger.Sync(); err != nil {
+		l.sugarLogger.Error(err)
+	}
+}
